6c3a3fc3f94d48b666b53cb2ac99d87a223e225c
ricardo-mbancho <fkondem@gmail.com>
2026-08-05 02:15:12 +0200

    add: booking microservice

---
+++ microservices/booking-service/src/app.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (_, res) => {
+  res.json({
+    service: 'booking-service',
+    status: 'running',
+  });
+});
+
+
+export default app;
---
+++ microservices/booking-service/src/server.ts
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+import app from './app';
+
+const PORT = Number(process.env.PORT) || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Booking service running on ${PORT}`);
+});
